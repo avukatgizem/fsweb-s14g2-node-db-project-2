878bb9876fc7ff1b66e2ed6b698093fcc01588d3
avukatgizem <125642428+avukatgizem@users.noreply.github.com>
2023-06-02 10:12:27 +0300

    .

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,45 @@
-exports.up = function (knex) {
-  // SİHRİNİZİ GÖSTERİN
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable("cars",(table)=>{
+    table.increments(); // id isimli kolon oluşuyor. id 1-1 artan bir değer alıyor.
+    table.string("vin").notNullable().unique();
+    table.string("make").notNullable();
+    table.string("model").notNullable();
+    table.integer("mileage").notNullable();
+    table.string("title");
+    table.string("transmission");
+  });
 };
 
-exports.down = function (knex) {
-  // SİHRİNİZİ GÖSTERİN
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("cars");
+};/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable("cars",(table)=>{
+    table.increments(); // id isimli kolon oluşuyor. id 1-1 artan bir değer alıyor.
+    table.string("vin").notNullable().unique();
+    table.string("make").notNullable();
+    table.string("model").notNullable();
+    table.integer("mileage").notNullable();
+    table.string("title");
+    table.string("transmission");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("cars");
 };
